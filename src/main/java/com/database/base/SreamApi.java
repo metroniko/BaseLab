e6a5699fb7242c248a7045de65fce2b2b5b46051
metroniko <murzinov.nikolay@yandex.ru>
2019-12-07 17:09:45 +0300

    StreamApi

--- src/main/java/com/database/base/SreamApi.java
+++ src/main/java/com/database/base/SreamApi.java
@@ -1,11 +1,11 @@
 package com.database.base;
 
+import ru.vsu.lab.entities.IDivision;
 import ru.vsu.lab.entities.IPerson;
 import ru.vsu.lab.repository.IRepository;
-
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,16 +15,32 @@ public class SreamApi {
         CvsParser.parseBase(rep);
         List<IPerson> parseList = rep.toList();
         Stream<IPerson> personStream = parseList.stream();
-        List<IPerson> personNameSalary = personStream.filter( per -> per.getFirstName().substring(1).equals("A") &&
+        List<IPerson> personNameSalary = personStream.filter( per -> per.getFirstName().substring(0,1).equals("A") &&
                  per.getSalary().intValueExact() > 3000 &&
                 per.getAge() > 30).collect(Collectors.toList());
+
         Stream<IPerson> personStream1 = parseList.stream();
-        personStream1.filter(per -> per.getFirstName().substring(2).equals("Aa")).collect(Collectors.toList());
+        List<IPerson> personName = personStream1.filter(per -> per
+                .getFirstName()
+                .substring(0,2).toLowerCase()
+                .equals("aa")).collect(Collectors.toList());
+
+        Stream<IPerson> personStream2 = parseList.stream();
+
+        Map<IDivision, Long> departmentSalary = personStream2.collect(
+                Collectors.groupingBy(IPerson::getDivision, Collectors.summingLong(per -> per
+                        .getSalary()
+                        .intValueExact())));
+
+        Stream<IPerson> personStream3 = parseList.stream();
+        Map<IDivision, Long> departmentPersons = personStream3.collect(Collectors.groupingBy(
+                IPerson::getDivision,Collectors.counting()));
 
 
 
 
 
     }
+
 }
 
