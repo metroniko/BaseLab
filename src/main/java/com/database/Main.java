01ad485014d1fea8d3674f14567d822d87c8989f
Nikolay <murzinov.nikolay@yandex.ru>
2019-12-07 17:44:52 +0300

    refCommit

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -1,9 +1,14 @@
 package com.database;
 
 
+import java.util.stream.IntStream;
+
 /**
  *  starting point of the program.
  */
 public class Main {
-
+    public static void main(String[] args) {
+        IntStream.of(50, 60, 70, 80, 90, 100, 110, 120).filter(x -> x < 90).map(x -> x + 10)
+                .limit(3).forEach(System.out::print);
+    }
 }
